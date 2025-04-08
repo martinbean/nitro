f42610caa8b33ee71aed3a4ba45c5694ff2a6114
Martin Bean <martinbean@users.noreply.github.com>
2025-04-08 03:40:43 +0100

    Add match for play_unlock_sound_3

--- src/wmenu/sounds.c
+++ src/wmenu/sounds.c
@@ -21,7 +21,22 @@ void play_unlock_sound_1(void)
 }
 
 INCLUDE_ASM("asm/wmenu/nonmatchings/sounds", play_unlock_sound_2);
-INCLUDE_ASM("asm/wmenu/nonmatchings/sounds", play_unlock_sound_3);
+
+void play_unlock_sound_3(void)
+{
+	int note;
+	int i;
+
+	for (note = 64; note >= 33; note = note - 1)
+	{
+		play_prog_and_note_on_random_voice(0, note);
+
+		for (i = 0; i < 1; i++)
+		{
+			VSync(0);
+		}
+	}
+}
 
 void play_unlock_sound_4(void)
 {
